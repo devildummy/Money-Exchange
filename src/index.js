821d1b274ad5866bdf520b9a8b561f31eb028efd
devildummy <cyrok.13@gmail.com>
2018-03-17 02:08:43 +0300

    implement function in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,51 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  
+var coins = {};
+
+if (currency>10000){
+  coins.error="You are rich, my friend! We don't have so much coins for exchange";
+  return coins;
+}
+
+while (currency>=50) {
+  currency=currency-50;
+  if(isNaN(coins.H)){
+    coins.H=0;
+  }
+  coins.H++;
+}
+
+while (currency>=25) {
+  currency=currency-25;
+  if(isNaN(coins.Q)){
+    coins.Q=0;
+  }
+    coins.Q++;
+}
+
+while (currency>=10) {
+  currency=currency-10;
+  if(isNaN(coins.D)){
+    coins.D=0;
+  }
+  coins.D++;
+}
+
+while (currency>=5) {
+  currency=currency-5;
+  if(isNaN(coins.N)){
+    coins.N=0;
+  }
+  coins.N++;
+}
+
+while (currency>=1){
+  currency=currency-1;
+  if(isNaN(coins.P)){
+    coins.P=0;
+  }
+  coins.P++;
+}
+return coins;
 }
